bf0c75f53fd36cc3a4c9f5682e8a66cd4b3b1af9
arithmeticbird <arithmeticbird@gmail.com>
2016-02-01 01:08:58 +0530

    implementation of div

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,7 +5,12 @@ object Main extends App{
 
   //60 / (5 *(7 + -(7 + ("true".length==5)?2:6)))
   def expression[R[_]](implicit s1: ExprSym[R], s2:DivisonSym[R]): R[Int] = {
-   s2.div(s1.literal(60)) (s1.mul(s1.literal(5))(s1.add(s1.literal(7))(s1.negation(s1.add(s1.literal(7))(s1.cond(s1.literalBoolean("true".length == 5))(s1.literal(2))(s1.literal(6)))))))
+    s2
+      .div(s1.
+        literal(60))(s1.
+        mul(s1.literal(5))(s1
+          .add(s1.literal(7))(s1.negation(s1.add(s1.literal(7))(s1.cond(s1.
+            literalBoolean("true".length == 5))(s1.literal(2))(s1.literal(6)))))))
   }
 
   println(finalTagless.FinalEmbedding.eval(expression))
